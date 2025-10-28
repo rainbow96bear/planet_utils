57b3db2c8c91e76a83e0bb7f2aefdeae767f1486
rainbow96bear <rainbow96bear@gmail.com>
2025-10-28 17:50:05 +0900

    jwt 인증 util 추가

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,27 @@
+package jwt
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func ParseAndVerifyJWT(tokenStr, secretKey string) (map[string]interface{}, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("invalid claims")
+	}
+
+	return claims, nil
+}
