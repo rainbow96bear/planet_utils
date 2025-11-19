6b43e72502d6a364cca8759dcf2aaed335f138b2
rainbowbear <rainbow96bear@gmail.com>
2025-11-20 07:25:01 +0900

    oauth id 추가

--- models/user.go
+++ models/user.go
@@ -11,6 +11,7 @@ type User struct {
 	ID            uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	Email         string    `gorm:"unique;not null"`
 	OAuthProvider string
+	OAuthID       string
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 
